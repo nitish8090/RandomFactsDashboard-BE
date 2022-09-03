bbe1b90d1c5f333097da058521d243cfd91bbef0
Nitish-ROG <nitishpatel1234@gmail.com>
2022-09-04 04:30:23 +0530

    Added USER APIs

--- User/models.py
+++ User/models.py
@@ -1,3 +1,7 @@
+import os
+import csv
+import random
+from pathlib import Path
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -9,10 +13,42 @@ class User(db.Model):
     email = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
     reset_code = db.Column(db.String(120))
-    facts = db.relationship("Facts")
+    facts = db.relationship("Facts", cascade="all,delete")
+    is_admin = db.Column(db.Boolean, default=False)
+
+    # Extra information
+    address = db.Column(db.Text())
+    post_code = db.Column(db.Text())
+    landmark = db.Column(db.Text())
+    education = db.Column(db.Text())
+    country = db.Column(db.Text())
+    state = db.Column(db.Text())
+
+    def generate_starting_facts(self):
+        with open(os.path.join(Path(__file__).parent.parent, "random_facts.csv")) as csvfile:
+            random_facts = [_[0] for _ in csv.reader(csvfile)]
+
+            # Select 3 facts from the list at random
+            db.session.add_all([Facts(user=self.id, fact_text=_) for _ in random.sample(random_facts, 3)])
+
+        db.session.commit()
+
+    def serialize(self, include_password=False, include_facts=False, include_is_admin=False):
+        return_dict = {_: getattr(self, _) for _ in
+                       ('id', 'name', 'email', 'address', 'post_code', 'landmark', 'education', 'country', 'state')}
+        if include_password:
+            return_dict['password'] = self.password
+        if include_facts:
+            return_dict['facts'] = [{"id": f.id, "fact_text": f.fact_text} for f in self.facts]
+        if include_is_admin:
+            return_dict['is_admin'] = self.is_admin
+        return return_dict
 
 
 class Facts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     fact_text = db.Column(db.Text())
+
+    def serialize(self):
+        return {"id": self.id, "fact_text": self.fact_text}
--- User/routes.py
+++ User/routes.py
@@ -12,6 +12,15 @@ from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
+from functools import wraps
+
+from flask import Flask
+from flask import jsonify
+
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt
+from flask_jwt_extended import JWTManager
+from flask_jwt_extended import verify_jwt_in_request
 
 from . import auth_blueprint
 from .models import db, User, Facts
@@ -19,8 +28,33 @@ from .models import db, User, Facts
 from .email_module import send_forgot_password_link
 
 
+def admin_required():
+    """ Decorator function for admin access """
+
+    def wrapper(fn):
+        @wraps(fn)
+        def decorator(*args, **kwargs):
+            verify_jwt_in_request()
+            claims = get_jwt()
+            if claims["is_administrator"]:
+                return fn(*args, **kwargs)
+            else:
+                return jsonify(msg="Admins only!"), 403
+
+        return decorator
+
+    return wrapper
+
+
+""" ---------------------- Authentication routes ---------------------- """
+
+
 @auth_blueprint.route("/auth/register/", methods=["POST"])
 def register():
+    """
+    For registering new user
+    Upon registering, 3 random facts will be added to his account
+    """
     email = request.json.get("email")
     password = request.json.get("password")
     name = request.json.get("name")
@@ -33,19 +67,15 @@ def register():
     db.session.add(user)
     db.session.commit()
 
-    with open(os.path.join(Path(__file__).parent.parent, "random_facts.csv")) as csvfile:
-        random_facts = [_[0] for _ in csv.reader(csvfile)]
-
-        # Select 3 facts from the list at random
-        db.session.add_all([Facts(user=user.id, fact_text=_) for _ in random.sample(random_facts, 3)])
-
-    db.session.commit()
+    user.generate_starting_facts()
 
     return jsonify('Registered')
 
 
 @auth_blueprint.route("/auth/login/", methods=["POST"])
 def login():
+    """ Here auth token will be generated """
+
     email = request.json.get("email")
     password = request.json.get("password")
 
@@ -54,14 +84,19 @@ def login():
         return jsonify("User not found"), 401
 
     if user.password == password:
-        access_token = create_access_token(identity=email)
-        return jsonify(access_token=access_token)
+        if user.is_admin:
+            access_token = create_access_token(identity=email, additional_claims={"is_administrator": True})
+            return jsonify(access_token=access_token, is_rf_admin=True)
+        else:
+            access_token = create_access_token(identity=email)
+            return jsonify(access_token=access_token)
     else:
         return jsonify({"msg": "Bad username or password"}), 401
 
 
 @auth_blueprint.route("/auth/forgot-password/", methods=["POST"])
 def forgot_password():
+    """ Reset code will be generated here if forgot password. """
     email = request.json.get("email")
 
     user = User.query.filter_by(email=email).first()
@@ -94,17 +129,94 @@ def reset_password():
     return jsonify({"msg": "success"})
 
 
-@auth_blueprint.route("/protected", methods=["GET"])
+""" ---------------------- User routes ---------------------- """
+
+
+@auth_blueprint.route("/User/", methods=["GET", "POST"])
+@admin_required()
+def list_create_user():
+    # Access the identity of the current user with get_jwt_identity
+    # current_user = get_jwt_identity()
+
+    if request.method == 'GET':
+        users = User.query.all()
+        return jsonify([user.serialize(include_facts=True) for user in users])
+
+    elif request.method == 'POST':
+        user = User.query.filter_by(email=request.json.get('email')).first()
+        if user:
+            return jsonify("User with email already exists"), 400
+
+        try:
+            user = User(name=request.json['name'], email=request.json['email'], password='createduser')
+        except KeyError as e:
+            return jsonify(f"Please send key: {e}"), 400
+        else:
+            for f in ['address', 'post_code', 'landmark', 'education', 'country', 'state']:
+                if request.json.get(f):
+                    setattr(user, f, request.json.get(f))
+            db.session.add(user)
+            db.session.commit()
+            user.generate_starting_facts()
+            return jsonify(user.serialize(include_facts=True)), 201
+
+
+@auth_blueprint.route("/User/Profile/", methods=["GET", "PUT"])
 @jwt_required()
-def protected():
+def user_profile():
     # Access the identity of the current user with get_jwt_identity
     current_user = get_jwt_identity()
-    return jsonify(logged_in_as=current_user), 200
+
+    user = User.query.filter_by(email=current_user).first()
+    if request.method == 'GET':
+        return jsonify(user.serialize(include_facts=True))
+
+    elif request.method == 'PUT':
+        for f in ['name', 'address', 'post_code', 'landmark', 'education', 'country', 'state']:
+            if request.json.get(f):
+                setattr(user, f, request.json.get(f))
+        db.session.add(user)
+        db.session.commit()
+        return jsonify(user.serialize(include_facts=True)), 201
+
+
+@auth_blueprint.route("/User/<user_id>/", methods=["GET", "DELETE", "PUT"])
+@admin_required()
+def retrieve_delete_user(user_id):
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+
+    user = User.query.filter_by(id=user_id).first()
+    if not user:
+        return "User not found", 404
+
+    if request.method == 'GET':
+        return jsonify(user.serialize(include_facts=True, include_is_admin=True)), 200
+
+    elif request.method == "PUT":
+        user.is_admin = request.json.get('is_admin')
+
+        for f in ['name', 'email', 'password', 'address', 'post_code', 'landmark', 'education', 'country', 'state']:
+            if request.json.get(f):
+                setattr(user, f, request.json.get(f))
+
+        db.session.add(user)
+        db.session.commit()
+
+        return jsonify(user.serialize()), 201
+
+    elif request.method == 'DELETE':
+        if current_user == user.email:
+            return jsonify("Cannot delete self"), 409
+        db.session.delete(user)
+        db.session.commit()
+
+        return jsonify(f"{user.email} deleted"), 204
 
 
 @auth_blueprint.route("/User/GetFacts/", methods=["GET"])
 @jwt_required()
-def get_user():
+def get_user_facts():
     # Access the identity of the current user with get_jwt_identity
     current_user = get_jwt_identity()
 
@@ -112,12 +224,7 @@ def get_user():
     if not user:
         return "User not found", 404
 
-    response_data = {
-        "id": user.id,
-        "email": user.email,
-        "name": user.name,
-        "facts": [{"id": f.id, "fact_text": f.fact_text} for f in user.facts]
-    }
+    response_data = user.serialize(include_facts=True)
     return jsonify(response_data), 200
 
 
